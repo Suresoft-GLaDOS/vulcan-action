2e2be0723f63dbf333a988bce3517b265d47910e
Yeongcheol Kim <aree91@naver.com>
2022-02-23 15:10:49 +0900

    Fixed reference environment variable

--- vulcan/yaml/config.sh
+++ vulcan/yaml/config.sh
@@ -3,16 +3,16 @@
 # exist dependency
 wget https://github.com/mikefarah/yq/releases/download/v4.20.2/yq_linux_386 -O $GITHUB_ACTION_PATH/yq && chmod +x $GITHUB_ACTION_PATH/yq
 
-VULCAN_YML_NAME=$(GITHUB_ACTION_PATH/yq eval '.name')
-VULCAN_YML_URL=$(GITHUB_ACTION_PATH/yq eval '.url')
-VULCAN_YML_DOCKER_IMAGE=$(GITHUB_ACTION_PATH/yq eval '.docker-image')
-VULCAN_YML_EXTRA_BUILD_ENV_SETTING_COMMAND=$(GITHUB_ACTION_PATH/yq eval '.extra-build-env-setting-commands')
-VULCAN_YML_TEST_BUILD_COMMAND=$(GITHUB_ACTION_PATH/yq eval '.test-build-command')
-VULCAN_YML_COVERAGE_BUILD_COMMAND=$(GITHUB_ACTION_PATH/yq eval '.coverage-build-command')
-VULCAN_YML_TEST_TYPE=$(GITHUB_ACTION_PATH/yq eval '.test-type')
-VULCAN_YML_TEST_CASE=$(GITHUB_ACTION_PATH/yq eval '.test-case')
-VULCAN_YML_TEST_COMMAND=$(GITHUB_ACTION_PATH/yq eval '.test-command')
-VULCAN_YML_TEST_COVERAGE_COMMAND=$(GITHUB_ACTION_PATH/yq eval '.test-coverage-command')
+VULCAN_YML_NAME=$($GITHUB_ACTION_PATH/yq eval '.name')
+VULCAN_YML_URL=$($GITHUB_ACTION_PATH/yq eval '.url')
+VULCAN_YML_DOCKER_IMAGE=$($GITHUB_ACTION_PATH/yq eval '.docker-image')
+VULCAN_YML_EXTRA_BUILD_ENV_SETTING_COMMAND=$($GITHUB_ACTION_PATH/yq eval '.extra-build-env-setting-commands')
+VULCAN_YML_TEST_BUILD_COMMAND=$($GITHUB_ACTION_PATH/yq eval '.test-build-command')
+VULCAN_YML_COVERAGE_BUILD_COMMAND=$($GITHUB_ACTION_PATH/yq eval '.coverage-build-command')
+VULCAN_YML_TEST_TYPE=$($GITHUB_ACTION_PATH/yq eval '.test-type')
+VULCAN_YML_TEST_CASE=$($GITHUB_ACTION_PATH/yq eval '.test-case')
+VULCAN_YML_TEST_COMMAND=$($GITHUB_ACTION_PATH/yq eval '.test-command')
+VULCAN_YML_TEST_COVERAGE_COMMAND=$($GITHUB_ACTION_PATH/yq eval '.test-coverage-command')
 
 echo .vulcan.yml.name: ${VULCAN_YML_NAME:-Not set}
 echo .vulcan.yml.url: ${VULCAN_YML_URL:-Not set}
