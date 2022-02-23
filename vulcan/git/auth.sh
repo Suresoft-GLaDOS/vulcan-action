7403ad676f3c9d995aae888e0346e88286786d3e
Yeongcheol Kim <aree91@naver.com>
2022-02-23 11:35:29 +0900

    Fixed: Revised git token path

--- vulcan/git/auth.sh
+++ vulcan/git/auth.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 echo ==========gh auth login==========
-echo $TOKEN > /token
-gh auth login --with-token < /token
+echo $TOKEN > $GITHUB_ACTION_PATH/token
+gh auth login --with-token < $GITHUB_ACTION_PATH/token
 gh auth status
 echo ==================================
