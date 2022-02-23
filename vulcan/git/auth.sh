ef2f0bf55ef29960267a72a39fd0259481899ba6
Yeongcheol Kim <aree91@naver.com>
2022-02-23 12:30:43 +0900

    Fixed: Revised github token transfer

--- vulcan/git/auth.sh
+++ vulcan/git/auth.sh
@@ -2,7 +2,6 @@
 #!/bin/bash
 
 echo ==========gh auth login==========
-echo $TOKEN > $GITHUB_ACTION_PATH/token
 gh auth login --with-token < $GITHUB_ACTION_PATH/token
 gh auth status
 echo ==================================
