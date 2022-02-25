8da70e1b21e0a9f084f9bcead428ca2bf0efcfad
Yeongcheol Kim <aree91@naver.com>
2022-02-25 14:29:48 +0900

    Removed build status check code

--- vulcan/runner/run_fl.sh
+++ vulcan/runner/run_fl.sh
@@ -5,10 +5,10 @@ cd $GITHUB_WORKSPACE/vulcan_target
 
 echo "Run VULCAN_YML_COVERAGE_BUILD_COMMAND"
 $(sh -c "$(printf "$VULCAN_YML_COVERAGE_BUILD_COMMAND")")
-if [ ! $? -eq 0 ]; then
-  echo "Build failed"
-  exit 1
-fi
+# if [ ! $? -eq 0 ]; then
+#   echo "Build failed"
+#   exit 1
+# fi
 $(sh -c "$(printf "$VULCAN_YML_TEST_COVERAGE_COMMAND")")
 
 $GITHUB_ACTION_PATH/vulcan/bin/sbfl
