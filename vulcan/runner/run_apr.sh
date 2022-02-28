91cdb1c21f60698fb9c77cb23f8df0a84cb59152
Yeongcheol Kim <aree91@naver.com>
2022-02-28 10:00:48 +0900

    Fixed run command

--- vulcan/runner/run_apr.sh
+++ vulcan/runner/run_apr.sh
@@ -3,7 +3,10 @@
 
 cd $GITHUB_WORKSPACE/vulcan_target
 
-# $(sh -c "$(printf "$VULCAN_YML_TEST_BUILD_COMMAND")")
-# $(sh -c "$(printf "$VULCAN_YML_TEST_COMMAND")")
+echo "Run VULCAN_YML_TEST_BUILD_COMMAND"
+# sh -c "$VULCAN_YML_TEST_BUILD_COMMAND"
+
+echo "Run VULCAN_YML_TEST_COMMAND"
+# sh -c "$VULCAN_YML_TEST_COMMAND"
 
 $GITHUB_ACTION_PATH/vulcan/bin/msv
