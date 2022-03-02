89e6043a6ad897393094e7f208234a82e80ee6e6
Yeongcheol Kim <aree91@naver.com>
2022-03-02 15:14:02 +0900

    Feature gcov collect (#1)
    * Added gcov data collection code

--- vulcan/runner/run_fl.sh
+++ vulcan/runner/run_fl.sh
@@ -4,14 +4,14 @@
 cd $GITHUB_WORKSPACE/vulcan_target
 
 echo "Run VULCAN_YML_COVERAGE_BUILD_COMMAND"
-sh -c "$VULCAN_YML_COVERAGE_BUILD_COMMAND"
+sh -c "$VULCAN_YML_COVERAGE_BUILD_COMMAND" > /dev/null
 
 if [ ! $? -eq 0 ]; then
   echo "Build failed"
   exit 1
 fi
 
-echo "Run VULCAN_YML_TEST_COVERAGE_COMMAND"
-sh -c "$VULCAN_YML_TEST_COVERAGE_COMMAND"
+echo "Run split test by VULCAN_YML_TEST_COVERAGE_COMMAND"
+source $GITHUB_ACTION_PATH/vulcan/util/test.sh
 
 $GITHUB_ACTION_PATH/vulcan/bin/sbfl
---
+++ vulcan/util/test.sh
@@ -0,0 +1,41 @@
+# /vulcan/util/test.sh
+
+TEST_INDEX=1
+GCOV_PATH=$GITHUB_WORKSPACE/gcov
+
+_create_gcov_directory() {
+	rm -rf $GCOV_PATH
+	mkdir $GCOV_PATH
+}
+
+_write_test_result() {
+	if [ ! $? -eq 0 ];
+	then
+		echo fail > $GCOV_PATH/$TEST_INDEX/result.output
+	else
+		echo pass > $GCOV_PATH/$TEST_INDEX/result.output
+	fi
+}
+
+_clean_after_collect_gcov() {
+	find $GITHUB_WORKSPACE/vulcan_target ! \( -path '*test*' -prune \) -type f -name "*.o" -exec gcov --preserve-paths {} \; > /dev/null
+	mv $GITHUB_WORKSPACE/vulcan_target/*.gcov $GCOV_PATH/$TEST_INDEX
+	find $GITHUB_WORKSPACE/vulcan_target -type f -name "*.gcda" -delete
+}
+
+_split_test() {
+	for UNIT_TEST in $(sh -c "$VULCAN_YML_TEST_LIST | grep test_")
+	do
+		echo "Measuring coverage for $UNIT_TEST\n"
+		mkdir $GCOV_PATH/$TEST_INDEX
+		sh -c "${VULCAN_YML_TEST_COVERAGE_COMMAND//\?/$UNIT_TEST}"
+		
+		_write_test_result
+		_clean_after_collect_gcov
+		
+		TEST_INDEX=$(( $TEST_INDEX + 1 ))
+	done
+}
+
+_create_gcov_directory
+_split_test
