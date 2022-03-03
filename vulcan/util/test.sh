e853e56c53b845e7d88e089553bd6f8045193e61
Yeongcheol Kim <aree91@naver.com>
2022-03-03 17:23:43 +0900

    Lcov Test

--- vulcan/util/test.sh
+++ vulcan/util/test.sh
@@ -18,8 +18,10 @@ _write_test_result() {
 }
 
 _clean_after_collect_gcov() {
-	find $GITHUB_WORKSPACE/vulcan_target ! \( -path '*test*' -prune \) -type f -name "*.o" -exec gcov --preserve-paths {} \; > /dev/null 2>/dev/null
-	mv $GITHUB_WORKSPACE/vulcan_target/*.gcov $GCOV_PATH/$TEST_INDEX
+	# find $GITHUB_WORKSPACE/vulcan_target ! \( -path '*test*' -prune \) -type f -name "*.o" -exec gcov --preserve-paths {} \; > /dev/null 2>/dev/null
+	# mv $GITHUB_WORKSPACE/vulcan_target/*.gcov $GCOV_PATH/$TEST_INDEX
+	lcov --directory=$GITHUB_WORKSPACE/vulcan_target --output-file $GCOV_PATH/$TEST_INDEX/generated.info --capture -f
+	genhtml $GCOV_PATH/$TEST_INDEX/generated.info --output-directory=$GCOV_PATH/$TEST_INDEX/html
 	find $GITHUB_WORKSPACE/vulcan_target -type f -name "*.gcda" -delete
 }
 
