1b31d00db06514d74209e793d21e28d182265ba9
Yeongcheol Kim <aree91@naver.com>
2022-03-04 11:59:39 +0900

    Fixed fl run command

--- vulcan/runner/run_fl.sh
+++ vulcan/runner/run_fl.sh
@@ -5,4 +5,4 @@
 
 # temp code
 cd /home/workspace/sbfl
-python3.9 -m sbfl -f Ochiai2 ../../runner/vulcan-demo/_work/output/$GITHUB_REPOSITORY/$VULCAN_SUFFIX/gcov > $VULCAN_OUTPUT_DIR/fl.output
+python3.9 -m sbfl -f Ochiai2 ../../runner/vulcan-demo/_work/output/$GITHUB_REPOSITORY/$VULCAN_SUFFIX/gcov/* > $VULCAN_OUTPUT_DIR/fl.output
