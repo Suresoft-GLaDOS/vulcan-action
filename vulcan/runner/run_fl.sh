e390c0044d8c5412ae445ddeaac40a7169aa6f01
Yeongcheol Kim <aree91@naver.com>
2022-03-04 11:57:39 +0900

    Fixed typo sfbl

--- vulcan/runner/run_fl.sh
+++ vulcan/runner/run_fl.sh
@@ -4,5 +4,5 @@
 # $GITHUB_ACTION_PATH/vulcan/bin/sbfl
 
 # temp code
-cd /home/workspace/sfbl
+cd /home/workspace/sbfl
 python3.9 -m sbfl -f Ochiai2 ../../runner/vulcan-demo/_work/output/$GITHUB_REPOSITORY/$VULCAN_SUFFIX/gcov > $VULCAN_OUTPUT_DIR/fl.output
