689fb0c24b7fc556ece599f946061d1ad1a2b48a
Yeongcheol Kim <aree91@naver.com>
2022-03-17 14:59:08 +0900

    Revised fl command

--- vulcan/runner/run_fl.sh
+++ vulcan/runner/run_fl.sh
@@ -6,4 +6,4 @@ echo "Find fault localization"
 
 # temp code
 cd /home/workspace/sbfl
-python3.9 -m sbfl -f Ochiai2 ../../runner/vulcan-demo/_work/output/$GITHUB_REPOSITORY/$VULCAN_SUFFIX/gcov/* --json-out $VULCAN_OUTPUT_DIR/fl.json
+python3.9 -m sbfl -f Ochiai2 ../../runner/vulcan-demo/_work/output/$GITHUB_REPOSITORY/$VULCAN_SUFFIX/gcov/* -s $VULCAN_OUTPUT_DIR/fl.json -i $VULCAN_OUTPUT_DIR/info.json
