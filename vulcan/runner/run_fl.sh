0305cd5f08d717532e71ef20aca5405562385fb1
Yeongcheol Kim <aree91@naver.com>
2022-03-18 10:52:41 +0900

    Modified relative path to absolute path

--- vulcan/runner/run_fl.sh
+++ vulcan/runner/run_fl.sh
@@ -5,5 +5,4 @@
 echo "Find fault localization"
 
 # temp code
-cd /home/workspace/sbfl
-python3.9 -m sbfl -f Ochiai2 ../../runner/vulcan-demo/_work/output/$GITHUB_REPOSITORY/$VULCAN_SUFFIX/gcov/* -s $VULCAN_OUTPUT_DIR/fl.json -i $VULCAN_OUTPUT_DIR/info.json
+python3.9 -m sbfl -f Ochiai2 $GCOV_PATH/* -s $VULCAN_OUTPUT_DIR/fl.json -i $VULCAN_OUTPUT_DIR/info.json
