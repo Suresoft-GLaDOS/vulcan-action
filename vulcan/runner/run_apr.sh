c1f2edbe7cc882aabaf13538da8b67bc742626b0
Yeongcheol Kim <aree91@naver.com>
2022-03-28 16:19:45 +0900

    Printed msv command

--- vulcan/runner/run_apr.sh
+++ vulcan/runner/run_apr.sh
@@ -5,11 +5,13 @@ $GITHUB_ACTION_PATH/vulcan/bin/msv
 echo "Find repair code"
 
 # temp
+cd $VULCAN_OUTPUT_DIR
 MSV_REPO=/home/workspace/msv
 MSV_SEARCH_REPO=/home/workspace/msv-search
 MSV_WORKSPACE=$VULCAN_OUTPUT_DIR/msv-workspace
 VULCAN_TARGET_WORKDIR=$MSV_WORKSPACE/$VULCAN_TARGET_NAME-workdir
 mkdir -p $MSV_WORKSPACE
+echo python3 $MSV_REPO/msv-runner.py -r $VULCAN_TARGET $MSV_WORKSPACE $MSV_REPO
 python3 $MSV_REPO/msv-runner.py -r $VULCAN_TARGET $MSV_WORKSPACE $MSV_REPO
 python3 $MSV_SEARCH_REPO/msv-search.py -o $VULCAN_OUTPUT_DIR/msv-output -w $VULCAN_TARGET_WORKDIR -m prophet -t 600 -p $MSV_REPO -- $MSV_REPO/tools/msv-test.py $VULCAN_TARGET_WORKDIR/src $VULCAN_TARGET_WORKDIR/tests $VULCAN_TARGET_WORKDIR
 
