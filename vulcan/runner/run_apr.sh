9c8843385b861af54d6cf34d365eea7e3806d1a9
Yeongcheol Kim <aree91@naver.com>
2022-03-28 15:33:56 +0900

    Added MSV script

--- vulcan/runner/run_apr.sh
+++ vulcan/runner/run_apr.sh
@@ -1,14 +1,20 @@
 # vulcan/runner/run_apr.sh
 #!/bin/bash
 
-PATCH_OUTPUT_PATH=$VULCAN_OUTPUT_DIR/patch
-
-cd $VULCAN_TARGET
-echo "Find repair code"
-mkdir -p $PATCH_OUTPUT_PATH
 $GITHUB_ACTION_PATH/vulcan/bin/msv
+echo "Find repair code"
 
 # temp
+MSV_REPO=/home/workspace/msv
+MSV_SEARCH_REPO=/home/workspace/msv-search
+MSV_WORKSPACE=$VULCAN_OUTPUT_DIR/msv-workspace
+VULCAN_TARGET_WORKDIR=$MSV_WORKSPACE/$VULCAN_TARGET_NAME-workdir
+mkdir -p $MSV_WORKSPACE
+python3 $MSV_REPO/msv-runner.py -r $VULCAN_TARGET $MSV_WORKSPACE $MSV_REPO
+python3 $MSV_SEARCH_REPO/msv-search.py -o $VULCAN_OUTPUT_DIR/msv-output -w $VULCAN_TARGET_WORKDIR -m prophet -t 600 -p $MSV_REPO -- $MSV_REPO/tools/msv-test.py $VULCAN_TARGET_WORKDIR/src $VULCAN_TARGET_WORKDIR/tests $VULCAN_TARGET_WORKDIR
+
+PATCH_OUTPUT_PATH=$VULCAN_OUTPUT_DIR/patch
+mkdir -p $PATCH_OUTPUT_PATH
 if [ -f $VULCAN_TARGET/patch/*-0001-*.diff ]; then
     cp -r $VULCAN_TARGET/patch $VULCAN_OUTPUT_DIR
 fi
