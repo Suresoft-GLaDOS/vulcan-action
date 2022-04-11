af25dad37d0a6726b1f11ca3cc0842ee70b2369e
Yeongcheol Kim <aree91@naver.com>
2022-04-11 11:37:47 +0900

    TEMP: printed python path

--- vulcan/runner/run_fl.sh
+++ vulcan/runner/run_fl.sh
@@ -7,4 +7,5 @@ echo "Find fault localization"
 # temp code
 cd /home/workspace/sbfl
 # python3 -m pip install -r requirements.txt
+which python3
 python3 -m sbfl -f Ochiai2 $GCOV_PATH/* -s $VULCAN_OUTPUT_DIR/fl.json -i $VULCAN_OUTPUT_DIR/info.json
