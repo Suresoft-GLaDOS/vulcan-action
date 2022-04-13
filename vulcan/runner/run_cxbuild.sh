74128a0ec9688c6bc976e669195ab491b20e538e
Yeongcheol Kim <aree91@naver.com>
2022-04-13 16:09:11 +0900

    Revised to set environment variable

--- vulcan/runner/run_cxbuild.sh
+++ vulcan/runner/run_cxbuild.sh
@@ -6,5 +6,5 @@ cd $VULCAN_TARGET_WORKDIR/src
 git clean -f -d
 set -a
 CXBUILD_OUTPUT_DIR=$VULCAN_OUTPUT_DIR
-python3 /home/workspace/cxbuild/cxbuild.py capture make LDFLAGS="-Wl,-rpath=$MSV_REPO/src/.libs -L$MSV_REPO/src/.libs -ltest_runtime"
 set +a
+python3 /home/workspace/cxbuild/cxbuild.py capture make LDFLAGS="-Wl,-rpath=$MSV_REPO/src/.libs -L$MSV_REPO/src/.libs -ltest_runtime"
