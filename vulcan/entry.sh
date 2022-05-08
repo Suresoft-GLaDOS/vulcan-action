63444581231f24932b309c41a4b36b85b0a3c661
Yeongcheol Kim <aree91@naver.com>
2022-05-09 08:47:27 +0900

    Fixed

--- vulcan/entry.sh
+++ vulcan/entry.sh
@@ -1,6 +1,8 @@
 # /entry.sh
 #!/bin/bash
 
+echo "[DEBUG] GITHUB_ACTION_PATH: $GITHUB_ACTION_PATH"
+
 source $GITHUB_ACTION_PATH/vulcan/git/config.sh
 source $GITHUB_ACTION_PATH/vulcan/git/checkout.sh
 
