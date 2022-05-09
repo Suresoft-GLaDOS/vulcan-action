caf1a4f01a3e5c9f3ebe166df27ed823f315fe2d
Yeongcheol Kim <aree91@naver.com>
2022-05-09 14:26:03 +0900

    Fixed

--- vulcan/runner/run_fl.sh
+++ vulcan/runner/run_fl.sh
@@ -8,4 +8,4 @@ echo "Find fault localization"
 cd $SBFL_REPO
 # python3 -m pip install -r requirements.txt
 echo [DEBUG] python3 -m sbfl -f Ochiai2 $VULCAN_OUTPUT_DIR/gcov/* -s $VULCAN_OUTPUT_DIR/fl.json -i $VULCAN_OUTPUT_DIR/info.json -c $VULCAN_OUTPUT_DIR/fl_cluster.json
-python3 -m sbfl -f Ochiai2 $VULCAN_OUTPUT_DIR/gcov/* -s $VULCAN_OUTPUT_DIR/fl.json -i $VULCAN_OUTPUT_DIR/info.json -c $VULCAN_OUTPUT_DIR/fl_cluster.json
+python3 -m sbfl -f Ochiai2 $VULCAN_OUTPUT_DIR/gcov/* -s $VULCAN_OUTPUT_DIR/fl.json -i $VULCAN_OUTPUT_DIR/info.json
