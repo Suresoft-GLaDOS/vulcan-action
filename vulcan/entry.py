7fb480a8a78e659065300194178272010f0f5872
Yeongcheol Kim <aree91@naver.com>
2022-05-13 16:40:38 +0900

    Added GITHUB_ACTOR to output directory

--- vulcan/entry.py
+++ vulcan/entry.py
@@ -5,13 +5,14 @@ import sys
 import yaml
 
 GITHUB_ACTION_PATH = os.getenv("GITHUB_ACTION_PATH")
+GITHUB_ACTOR = os.getenv("GITHUB_ACTOR")
 GITHUB_REPOSITORY = os.getenv("GITHUB_REPOSITORY")
 GITHUB_WORKSPACE = os.getenv("GITHUB_WORKSPACE")
 VULCAN_TARGET_NAME = os.getenv("VULCAN_TARGET")
 VULCAN_TARGET = os.path.join(GITHUB_WORKSPACE, VULCAN_TARGET_NAME)
 VULCAN_YML_PATH = os.path.join(VULCAN_TARGET, "vulcan.yml")
 VULCAN_SUFFIX = datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')
-VULCAN_OUTPUT_DIR = os.path.realpath( os.path.join(GITHUB_WORKSPACE, "..", "..", "output", GITHUB_REPOSITORY, VULCAN_SUFFIX) )
+VULCAN_OUTPUT_DIR = os.path.realpath( os.path.join(GITHUB_WORKSPACE, "..", "..", "output", GITHUB_ACTOR, GITHUB_REPOSITORY, VULCAN_SUFFIX) )
 
 # reset some environment variables
 os.environ["VULCAN_TARGET_NAME"] = VULCAN_TARGET_NAME
