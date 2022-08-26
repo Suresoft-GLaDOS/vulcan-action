664bc91f80aaaa8baf25a5ba67198f2ec1c08fe0
BaeEB <ebbae@suresofttech.com>
2022-08-26 11:36:06 +0900

    Add patch preprocessing

---
+++ vulcan/util/post_processing.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+import pathlib
+import argparse
+import logging
+import json
+import glob
+
+
+VULCAN_OUTPUT_DIR = os.getenv("VULCAN_OUTPUT_DIR")
+
+
+def post_processing():
+    patch_dir = os.path.join(VULCAN_OUTPUT_DIR, "patch")
+    patch_list = glob.glob(patch_dir)
+    for patch in patch_list:
+        print("Postprocessing this: " + patch)
+        os.system(f"sed -i 's/((void *)0)/NULL/g' {patch}")
+
+
+post_processing()
