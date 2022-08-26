4004559b6902fc910d680593d7847f018f3f90b4
BaeEB <ebbae@suresofttech.com>
2022-08-26 12:25:52 +0900

    Get path of each patch

--- vulcan/util/post_processing.py
+++ vulcan/util/post_processing.py
@@ -12,7 +12,7 @@ VULCAN_OUTPUT_DIR = os.getenv("VULCAN_OUTPUT_DIR")
 
 def post_processing():
     patch_dir = os.path.join(VULCAN_OUTPUT_DIR, "patch")
-    patch_list = glob.glob(patch_dir)
+    patch_list = glob.glob(patch_dir+"/*.patch")
     for patch in patch_list:
         print("Postprocessing this: " + patch)
         os.system(f"sed -i 's/((void *)0)/NULL/g' {patch}")
