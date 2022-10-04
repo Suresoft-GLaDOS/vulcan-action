761b6df4e3f7b206c1aee75a880fd4451b1f40f7
BaeEB <ebbae@suresofttech.com>
2022-10-04 11:59:52 +0900

    Print target list for debug

--- vulcan/util/gcovg.py
+++ vulcan/util/gcovg.py
@@ -105,7 +105,7 @@ def main():
     # glob all file's list
     target_file_list = []
     print('Args file')
-    print(args.file)
+    print(root_dir.rglob(args.file[0]))
     if len(inclusion_list) != 0:
         # print(inclusion_list)
         for file in args.file:
