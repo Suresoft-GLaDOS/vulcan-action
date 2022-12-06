10b21b012124933d0754bad8640867e533e27f85
BaeEB <ebbae@suresofttech.com>
2022-12-06 14:10:43 +0900

    Print msv args

--- vulcan/runner/runner.py
+++ vulcan/runner/runner.py
@@ -93,7 +93,7 @@ def run_apr():
     run_cmd = f"python3 {MSV_REPO}/msv-runner.py"
     msv_options = f" -s {MUTABLE_ENV['FL_JSON']} -r "
 
-    msv_args = f"{VULCAN_TARGET} {MUTABLE_ENV['MSV_WORKSPACE']} {MSV_REPO}"
+    msv_args = f"{VULCAN_TARGET} {MUTABLE_ENV['MSV_WORKSPACE']} {MSV_REPO} "
 
     if VULCAN_YML_SUBDIR != '':
         msv_options = msv_options + f"-d {VULCAN_YML_SUBDIR} "
@@ -102,10 +102,10 @@ def run_apr():
 
     print(f"[DEBUG] {msv_args}", flush=True)
 
-    msv_runner_cmd = run_cmd + msv_options + msv_args
-    msv_runner_cmd = msv_runner_cmd + f"{VULCAN_TARGET} {MUTABLE_ENV['MSV_WORKSPACE']} {MSV_REPO}"
-    print(f"[DEBUG] {msv_runner_cmd}", flush=True)
-    ret_meta = os.system(msv_runner_cmd)
+    msv_runner_cmd = run_cmd + msv_options
+    msv_runner_cmd_arg = msv_runner_cmd + f"{VULCAN_TARGET} {MUTABLE_ENV['MSV_WORKSPACE']} {MSV_REPO}"
+    print(f"[DEBUG] {msv_runner_cmd_arg}", flush=True)
+    ret_meta = os.system(msv_runner_cmd_arg)
     handle_error(ret_meta, "apr-runner return non-zero",
                  additional_command=f"cat {MUTABLE_ENV['MSV_WORKSPACE']}/output.log")
 
