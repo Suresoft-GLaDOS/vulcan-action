81df27fac981c66be6a5fbc1b1533c359f98f3de
BaeEB <ebbae@suresofttech.com>
2022-12-06 11:04:39 +0900

    Especially include for libchewing

--- vulcan/util/gcovg.py
+++ vulcan/util/gcovg.py
@@ -117,6 +117,14 @@ def main():
                 print("Check inclusion: " + p)
                 if p in inclusion_list[0]:
                     target_file_list.append(p)
+    elif 'chewing' in VULCAN_YML_GCOV_INCLUSION_LIST:
+        for file in args.file:
+            for p in root_dir.rglob(file):
+                print("Check inclusion: " + p)
+                if VULCAN_YML_GCOV_INCLUSION_LIST in p:
+                    print("Libchewing inclusion: " + p)
+                    target_file_list.append(p)
+
     else:
         for file in args.file:
             for p in root_dir.rglob(file):
